45bdfcf17e397f853c40bcb77478bcb2d1b4ee9b
George Busby <gbusby01@protonmail.com>
2025-11-25 13:05:56 +0000

    Create carousel slide data model for carousel data

---
+++ lib/models/carousel_slide.dart
@@ -0,0 +1,12 @@
+/// Represents a single slide in the carousel
+class CarouselSlide {
+  final String imageUrl;
+  final String title;
+  final String subtitle;
+
+  const CarouselSlide({
+    required this.imageUrl,
+    required this.title,
+    required this.subtitle,
+  });
+}
