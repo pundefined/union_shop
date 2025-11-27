4e9d041cb0192843c0e156d2f50b4331b5d19bf8
George Busby <gbusby01@protonmail.com>
2025-11-27 21:54:44 +0000

    Refactor CollectionPage to support sorting by name and price

--- lib/screens/collection_page.dart
+++ lib/screens/collection_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:union_shop/models/collection.dart';
+import 'package:union_shop/models/product.dart';
 import 'package:union_shop/styles/text_styles.dart';
 import 'package:union_shop/widgets/control_section.dart';
 import 'package:union_shop/widgets/product_card.dart';
@@ -11,13 +12,54 @@ import 'package:union_shop/widgets/product_card.dart';
 /// - Filter and sort control section
 /// - Responsive grid (2 columns on mobile, 3 on tablet/desktop)
 /// - Product cards with tap feedback
-class CollectionPage extends StatelessWidget {
+/// - Sorting functionality (by name and price)
+class CollectionPage extends StatefulWidget {
   final Collection collection;
 
   const CollectionPage({
     super.key,
     required this.collection,
   });
+
+  @override
+  State<CollectionPage> createState() => _CollectionPageState();
+}
+
+class _CollectionPageState extends State<CollectionPage> {
+  late List<Product> _sortedProducts;
+  String _currentSort = 'name';
+
+  @override
+  void initState() {
+    super.initState();
+    _sortedProducts = List.from(widget.collection.items);
+    _applySorting(_currentSort);
+  }
+
+  void _applySorting(String sortOption) {
+    setState(() {
+      _currentSort = sortOption;
+      switch (sortOption) {
+        case 'name':
+          _sortedProducts.sort((a, b) => a.name.compareTo(b.name));
+          break;
+        case 'price_low':
+          _sortedProducts.sort((a, b) {
+            final priceA = a.discountedPrice ?? a.price;
+            final priceB = b.discountedPrice ?? b.price;
+            return priceA.compareTo(priceB);
+          });
+          break;
+        case 'price_high':
+          _sortedProducts.sort((a, b) {
+            final priceA = a.discountedPrice ?? a.price;
+            final priceB = b.discountedPrice ?? b.price;
+            return priceB.compareTo(priceA);
+          });
+          break;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,19 +74,22 @@ class CollectionPage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  collection.title,
+                  widget.collection.title,
                   style: TextStyles.collectionPageHeading,
                 ),
                 const SizedBox(height: 8),
                 Text(
-                  collection.description,
+                  widget.collection.description,
                   style: TextStyles.collectionPageDescription,
                 ),
               ],
             ),
           ),
           // Control Section
-          const ControlSection(),
+          ControlSection(
+            currentSort: _currentSort,
+            onSortChanged: _applySorting,
+          ),
           // Products Grid
           Padding(
             padding: const EdgeInsets.all(16),
@@ -77,9 +122,9 @@ class CollectionPage extends StatelessWidget {
         crossAxisSpacing: 16,
         childAspectRatio: 0.75,
       ),
-      itemCount: collection.items.length,
+      itemCount: _sortedProducts.length,
       itemBuilder: (context, index) {
-        final product = collection.items[index];
+        final product = _sortedProducts[index];
         return ProductCard(product: product);
       },
     );
