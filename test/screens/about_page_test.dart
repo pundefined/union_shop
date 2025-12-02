3aa6309560f9213e967bf85997e9a4ab9e633a1d
George Busby <gbusby01@protonmail.com>
2025-12-02 20:18:53 +0000

    Refactor AboutPage widget test to use widget test helper

--- test/screens/about_page_test.dart
+++ test/screens/about_page_test.dart
@@ -1,26 +1,23 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:union_shop/main.dart';
+import 'package:union_shop/screens/about_page.dart';
+
+import '../helpers/widget_test_helper.dart';
 
 void main() {
-  group('About Page Tests', () {
-    testWidgets('navigates to /about and shows content', (tester) async {
-      await tester.pumpWidget(const UnionShopApp());
+  group('AboutPage Widget Tests', () {
+    testWidgets('displays title and content', (tester) async {
+      await tester.setViewportSize(TestViewportSizes.desktop);
+
+      await tester.pumpWidget(wrapWidgetScrollable(const AboutPage()));
       await tester.pumpAndSettle();
 
-      // The About button should be present in the navbar
-      final aboutFinder = find.text('About');
-      expect(aboutFinder, findsOneWidget);
-
-      // Tap the About button to navigate
-      await tester.tap(aboutFinder);
-      await tester.pumpAndSettle();
-
-      // Verify the About page title is shown and at least one paragraph
       expect(find.text('About us'), findsOneWidget);
       expect(
         find.textContaining('Welcome to the Union Shop!'),
         findsOneWidget,
       );
+
+      await tester.resetViewportSize();
     });
   });
 }
